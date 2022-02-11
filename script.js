483156e70b3d0f85412fb33b49e828b115ba1488
LaMarr Smith <ljsmith19@my.trine.edu>
2022-02-11 17:32:24 -0500

    Still no luck on making it change color On hover/click in JS

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 const gridContainer = document.getElementById("grid-container");
 const root = document.querySelector("root");
-let gridCell = document.getElementsByClassName("gridCell");
+let gridCell = document.getElementById("gridCell");
+let gridClass = document.getElementsByClassName("gridCell");
 
 // Function that makes the grid
 function makeG(rowNum, colNum){
     for (let i = 0; i < (rowNum * colNum); i++){
         let gridCell = document.createElement('div');
         gridContainer.appendChild(gridCell).id = "gridCell";
+        gridCell.innerHTML = i;
         gridCell.classList = "gridCell" + i;
     };
 };
-// console.log(gridCell.length);
+
+// function changeColor(){
+//     gridCell.style.backgroundColor = "red";
+// };
+// changeColor();
+// for (let q = 0; q < gridClass.length;){
+//     gridCell.addEventListener("mouseover", changeColor());
+// };
 
 
-// for (let l = 0; l < gridCell.length; l++){
-//         let newColor = gridCell.item[0];
-// }
+
 makeG(16,16);
