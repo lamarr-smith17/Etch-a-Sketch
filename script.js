f3aec0cf77dbf0e7f74f68b9a4e338129c92502b
LaMarr Smith <ljsmith19@my.trine.edu>
2022-02-10 19:20:23 -0500

    Added a hover state in css Trying to make it an eventlistner

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 const gridContainer = document.getElementById("grid-container");
 const root = document.querySelector("root");
-let gridCell = document.getElementById("gridCell");
-
-console.log(gridContainer);
-console.log(gridCell);
+let gridCell = document.getElementsByClassName("gridCell");
 
 // Function that makes the grid
 function makeG(rowNum, colNum){
     for (let i = 0; i < (rowNum * colNum); i++){
         let gridCell = document.createElement('div');
-        gridCell.innerHTML = "" + i;
         gridContainer.appendChild(gridCell).id = "gridCell";
+        gridCell.classList = "gridCell" + i;
     };
 };
+// console.log(gridCell.length);
 
-// gridCell.addEventListener("click", ()=>{
-//     gridCell.style.color = "#C6E2FF";
-// });
+
+// for (let l = 0; l < gridCell.length; l++){
+//         let newColor = gridCell.item[0];
+// }
 makeG(16,16);
