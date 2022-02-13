b321b9e67be29999b1efa31bb7bfbd0731e04200
LaMarr Smith <ljsmith19@my.trine.edu>
2022-02-13 17:01:16 -0500

    Figured out how to add hover state to stay By using querySelectorAll and ForEach Trying to figure out how to clear the board

--- script.js
+++ script.js
@@ -1,26 +1,22 @@
 const gridContainer = document.getElementById("grid-container");
 const root = document.querySelector("root");
-let gridCell = document.getElementById("gridCell");
-let gridClass = document.getElementsByClassName("gridCell");
+const clearGridBtn = document.querySelector("#clear-btn");
 
 // Function that makes the grid
 function makeG(rowNum, colNum){
     for (let i = 0; i < (rowNum * colNum); i++){
         let gridCell = document.createElement('div');
-        gridContainer.appendChild(gridCell).id = "gridCell";
-        gridCell.innerHTML = i;
-        gridCell.classList = "gridCell" + i;
+        gridContainer.appendChild(gridCell).id = "gridCell";    
     };
+    let gridCellSelector = document.querySelectorAll("#gridCell"); // Selects all of the gridCell ID's and puts them into an array
+    gridCellSelector.forEach(changeColor); // For each element that has the ID of gridCell, the changeColor function is applied
 };
 
-// function changeColor(){
-//     gridCell.style.backgroundColor = "red";
-// };
-// changeColor();
-// for (let q = 0; q < gridClass.length;){
-//     gridCell.addEventListener("mouseover", changeColor());
-// };
-
+function changeColor(gridCellSelector){
+    gridCellSelector.addEventListener('mouseover', (changeColor) =>{
+        changeColor.target.classList.add("colorChange");
+    })
+};
 
 
 makeG(16,16);
